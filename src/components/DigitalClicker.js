6d3cf48196cbbf49fd30919b4ab14c9f6976c72c
RyanBegnaud <Ryanbegnaud13@gmail.com>
2021-03-10 22:41:44 -0800

    commit tree 79641ef556621e8407ad21966c2a1c24a21370d8

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
-// Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor(){
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    changeTimes = () => {
+        const newCount = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: newCount
+        })
+    }
+
+    render() {
+        return(
+            <button onClick={this.changeTimes}>{this.state.timesClicked}</button>
+        )
+    }
+} 
